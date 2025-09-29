00df3e1afb8827231ee9699ea4930f9ec6a891cd
Kshitij Bhusal <kshitijbhusal64@gmail.com>
2025-09-29 09:41:58 +0545

    working bot

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res)=> {
-    res.send("Hello World")
-})
+// index.js
+import TelegramBot from "node-telegram-bot-api";
+import dotenv from "dotenv"
 
 
-app.listen(3001, ()=>{
-    console.log("server started ");
-    
+
+dotenv.config({path:'./.env'}); // load .env file
+
+
+console.log(process.env.BOT_TOKEN);
+
+// Replace with your BotFather token
+const TELEGRAM_TOKEN = process.env.BOT_TOKEN;
+
+// Create bot with polling enabled
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
+
+// Listen for messages
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const userText = msg.text;
+
+  // Reply back
+  bot.sendMessage(chatId, `You said: ${userText}`);
 });
+
+// You can also add commands
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "👋 Hello! this is a steezcodes bot.");
+});
+
+console.log("🤖 Bot is running with polling...");
